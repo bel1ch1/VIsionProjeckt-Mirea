48b9e835039aaf90e6156382050366326e746b3b
AndreyZ <andreyzv2525@gmail.com>
2024-03-28 11:30:49 +0300

    add video view class

--- Views/Video_View.py
+++ Views/Video_View.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import cv2
+
 
 class Video_View:
-    def __init__(self):
-        pass
+    def __init__(self, camera_id=0, camera_name="test"):
+        self.camera_id = camera_id
+        self.camera_name = camera_name
+
+    def video_output(camera_id, camera_name):
+        cap = cv2.VideoCapture(camera_id)
+        while True:
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            cv2.imshow(camera_name, frame)
+            cap.release()
+            cv2.destroyAllWindows()
