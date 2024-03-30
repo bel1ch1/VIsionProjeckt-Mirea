46b5025688b08521bfc5cee8eaff7724a0f0514c
AndreyZ <andreyzv2525@gmail.com>
2024-03-30 13:45:12 +0300

    changed the registration form

--- streamlit_scripts/Register.py
+++ streamlit_scripts/Register.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from DataBase.db_manager import register_user
 
 
 # Форма регистрации
-st.subheader('Регистрация')
-new_username = st.text_input('Введите новое имя пользователя')
-new_password = st.text_input('Введите новый пароль', type='password')
+def register_form():
+    st.subheader('Регистрация')
+    new_username = st.text_input('Введите новое имя пользователя')
+    new_password = st.text_input('Введите новый пароль', type='password')
+    if st.button('Зарегистрировать'):
+        register_user(new_username, new_password)
