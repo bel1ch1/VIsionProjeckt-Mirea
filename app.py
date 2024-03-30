8c9b2eba66c1d35aa3275ccf658f1fc19d8e7356
AndreyZ <andreyzv2525@gmail.com>
2024-03-30 13:22:09 +0300

    change app.py

--- app.py
+++ app.py
@@ -1,31 +1,8 @@
 import streamlit as st
-import sqlite3
-from passlib.hash import pbkdf2_sha256
 import os
+from DataBase.db_manager import register_user, login_user
 
-# Создаем соединение с базой данных (или открываем существующую базу данных)
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-
-# Создаем таблицу пользователей (если она еще не существует)
-c.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)''')
-
-# Функция для регистрации нового пользователя
-def register_user(username, password):
-    hashed_password = pbkdf2_sha256.hash(password)
-    c.execute('''INSERT INTO users (username, password) VALUES (?, ?)''', (username, hashed_password))
-    conn.commit()
-    st.success("Пользователь зарегистрирован")
-
-# Функция для аутентификации пользователя
-def login_user(username, password):
-    c.execute('''SELECT password FROM users WHERE username = ?''', (username,))
-    result = c.fetchone()
-    if result and pbkdf2_sha256.verify(password, result[0]):
-        st.success("Успешная аутентификация")
-    else:
-        st.warning("Неверное имя пользователя или пароль")
-
+# Рисуем
 
 # Форма для регистрации нового пользователя
 st.subheader('Регистрация')
