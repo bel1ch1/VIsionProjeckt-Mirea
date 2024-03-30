4349dd7fff4177a134b6a6a479533362bb650d50
AndreyZ <andreyzv2525@gmail.com>
2024-03-30 16:03:28 +0300

    end

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 # Рисуем сайт подставляя формы из streamlit_scripts
 
-
+# Импортируем
 import streamlit as st
 from streamlit_scripts.Login import login_form
 from streamlit_scripts.Register import register_form
+from streamlit_scripts.ViewDetector import view_detector
 
 
-# Форма для регистрации нового пользователя
-register_form()
+with st.sidebar:
+    # Форма для регистрации нового пользователя
+    register_form()
+    # Форма для аутентификации пользователя
+    login_form()
 
-
-# Форма для аутентификации пользователя
-login_form()
+if st.session_state.log_in:
+    st.session_state['view'] = True
+    if st.session_state.view == True:
+        view_detector()
+    del st.session_state.view
