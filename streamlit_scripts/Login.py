80f573366a5ade1798e7dcd92e1bfbd8533ec731
AndreyZ <andreyzv2525@gmail.com>
2024-03-30 16:03:21 +0300

    end

--- streamlit_scripts/Login.py
+++ streamlit_scripts/Login.py
@@ -5,7 +5,11 @@ from DataBase.db_manager import login_user
 # Форма для логина
 def login_form():
     st.subheader('Вход')
-    login_username = st.text_input('Введите имя пользователя')
+    login_username = st.text_input('Введите имя пользователя',)
     login_password = st.text_input('Введите пароль', type='password')
-    if st.button('Войти'):
+    if st.button('Войти', key='log_in'):
         login_user(login_username, login_password)
+
+# def add_session():
+#     if 'btn_log' not in st.session_state:
+#         st.session_state.btn_log = True
